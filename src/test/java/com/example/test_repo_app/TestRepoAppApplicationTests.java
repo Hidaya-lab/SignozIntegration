3688817eca1ef58d0cce2ae156ba573c878d363a
hidayadhulla <uhidayadhulla@gmail.com>
2025-10-27 17:18:44 +0530

    dockerchange4

---
+++ src/test/java/com/example/test_repo_app/TestRepoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_repo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
